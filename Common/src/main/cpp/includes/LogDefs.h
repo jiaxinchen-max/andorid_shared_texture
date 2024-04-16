cbb856e5b8e62b43792d6a2bb1dcbcb24b8046bf
xinghua.chen <xinghua.chen@thundercomm.com>
2024-04-16 20:01:07 +0800

    update

--- Common/src/main/cpp/includes/LogDefs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <android/log.h>
-
-#define LOG_TAG "ndkbinderexamples"
-
-#define LOGD(fmt, ...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, fmt, ##__VA_ARGS__)
-
-#define LOGE(fmt, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, fmt, ##__VA_ARGS__)
--- Common/src/main/java/com/example/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-public interface Constants
-{
-    String LOG_TAG = "ndkbinderexamples";
-}
